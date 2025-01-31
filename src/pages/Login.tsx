e350a61913e49302b7bd09ce5cbf13d5939ab63d
alrayhan587 <alrayhan587@gmail.com>
2025-01-31 13:07:06 +0600

    Initial commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,68 @@
+import { Button } from 'antd';
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useLoginMutation } from '../redux/feature/auth/authApi';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../redux/feature/auth/authSlice';
+import { verifyToken } from '../util/verifyToken';
 
+/*
+type TuserInfo = {
+  id: string,
+  password: string,
+}
+  
+
+const [login, { data, error }] = useLoginMutation();
+console.log("data =>", data);
+console.log("error=>", error);
+
+*/
 const Login = () => {
+  const dispatch = useDispatch();
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: '0001',
+      password: 'admin12345'
+    }
+  });
+
+  const [login, { error }] = useLoginMutation();
+
+  // console.log('data=>', data);
+  // console.log('error=>', error);
+
+
+
+  const checkValue = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    }
+    // console.log(data);
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    console.log(res);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  }
+
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit(checkValue)}>
+      <div>
+        <label htmlFor='id'>Id:</label>.
+        <input type='text' id='id' {...register('id')} />
+
+      </div>
+
+      <div>
+        <label htmlFor='password'>Password:</label>.
+        <input type='text' id='password' {...register('password')} />
+      </div>
+
+      <Button htmlType='submit'>Login</Button>
+    </form>
   )
 }
 
---
+++ src/redux/feature/auth/authSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+type TAuthState = {
+    user: null | object,
+    token: null | string,
+}
+
+const initialState: TAuthState = {
+    user: null,
+    token: null,
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUser: (state: TAuthState, action) => {
+            const { user, token } = action.payload;
+            state.user = user;
+            state.token = token;
+        },
+
+        logout: (state: TAuthState) => {
+            state.user = null;
+            state.token = null;
+        }
+    }
+})
+
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
+
+
+//for protectedFile
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const selectCurrentUser = (state: RootState) => state.auth.user;
